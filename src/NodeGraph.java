bb28a88fcb90020a448a7831e1621d052ee722e0
Brendan <bwoods51501@gmail.com>
2023-04-10 16:37:40 -0400

    Fixed the moving issue it still highlights 3 as the fist option

--- src/NodeGraph.java
+++ src/NodeGraph.java
@@ -12,6 +12,13 @@ public class NodeGraph extends JFrame {
     private GraphPanel graphPanel;
     private int currentNode;
 
+    private final int NODE_SIZE = 30;
+
+    private int[][] adjacencyMatrix;
+    private int numNodes;
+    private Map<Integer, Point> nodePositions;
+
+
     public NodeGraph(int[][] adjacencyMatrix) {
         setTitle("Node Graph");
 
@@ -70,59 +77,85 @@ public class NodeGraph extends JFrame {
     private class GraphPanel extends JPanel {
 
         private final int NODE_SIZE = 30;
-
+    
         private int[][] adjacencyMatrix;
         private int numNodes;
         private Map<Integer, Point> nodePositions;
         private int currentNode;
-
+        private boolean positionsGenerated;
+    
         public GraphPanel(int[][] adjacencyMatrix) {
             this.adjacencyMatrix = adjacencyMatrix;
             this.numNodes = adjacencyMatrix.length;
             this.nodePositions = new HashMap<>();
+            this.positionsGenerated = false;
+        }
+        
+        @Override
+        public void addNotify() {
+            super.addNotify();
+    
+            if (isVisible()) {
+                generateNodePositions();
+                this.positionsGenerated = true;
+            }
         }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        Random random = new Random();
-        for (int i = 0; i < numNodes; i++) {
-            int x = random.nextInt(getWidth() - NODE_SIZE);
-            int y = random.nextInt(getHeight() - NODE_SIZE);
-            nodePositions.put(i, new Point(x, y));
+        private void generateNodePositions() {
+            // Generate random positions for the nodes
+            Random random = new Random();
+            for (int i = 0; i < numNodes; i++) {
+                int x = random.nextInt(getWidth() - NODE_SIZE);
+                int y = random.nextInt(getHeight() - NODE_SIZE);
+                nodePositions.put(i, new Point(x, y));
+            }
         }
 
-        for (int i = 0; i < numNodes; i++) {
-            for (int j = i + 1; j < numNodes; j++) {
-                if (adjacencyMatrix[i][j] == 1) {
-                    Point node1 = nodePositions.get(i);
-                    Point node2 = nodePositions.get(j);
+        @Override
+        public boolean isVisible() {
+            return super.isVisible() && getWidth() > 0 && getHeight() > 0;
+        }
 
-                if (visited.contains(i) && visited.contains(j)) {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            // Check if positions have been generated
+            if (!positionsGenerated) {
+                generateNodePositions();
+                this.positionsGenerated = true;
+            }
+    
+            for (int i = 0; i < numNodes; i++) {
+                for (int j = i + 1; j < numNodes; j++) {
+                    if (adjacencyMatrix[i][j] == 1) {
+                        Point node1 = nodePositions.get(i);
+                        Point node2 = nodePositions.get(j);
+    
+                        if (visited.contains(i) && visited.contains(j)) {
+                            g.setColor(Color.RED);
+                        } else {
+                            g.setColor(Color.BLACK);
+                        }
+                        g.drawLine(node1.x + NODE_SIZE / 2, node1.y + NODE_SIZE / 2,
+                                node2.x + NODE_SIZE / 2, node2.y + NODE_SIZE / 2);
+                    }
+                }
+            }
+    
+            for (int i = 0; i < numNodes; i++) {
+                Point node = nodePositions.get(i);
+                if (visited.contains(i)) {
                     g.setColor(Color.RED);
                 } else {
                     g.setColor(Color.BLACK);
                 }
-                g.drawLine(node1.x + NODE_SIZE / 2, node1.y + NODE_SIZE / 2,
-                        node2.x + NODE_SIZE / 2, node2.y + NODE_SIZE / 2);
-                }
-            }
-        }
-
-        for (int i = 0; i < numNodes; i++) {
-            Point node = nodePositions.get(i);
-            if (visited.contains(i)) {
-                g.setColor(Color.RED);
-            } else {
+                g.fillOval(node.x, node.y, NODE_SIZE, NODE_SIZE);
+                g.setColor(Color.WHITE);
+                g.drawString(Character.toString((char)(i+65)), node.x + NODE_SIZE / 2 - 4, node.y + NODE_SIZE / 2 + 4);
                 g.setColor(Color.BLACK);
             }
-            g.fillOval(node.x, node.y, NODE_SIZE, NODE_SIZE);
-            g.setColor(Color.WHITE);
-            g.drawString(Character.toString((char)(i+65)), node.x + NODE_SIZE / 2 - 4, node.y + NODE_SIZE / 2 + 4);
-            g.setColor(Color.BLACK);
-            }
         }
-    }
+    } 
 }
 
