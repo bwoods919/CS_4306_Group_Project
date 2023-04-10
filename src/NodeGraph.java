1b7f99e9b733fd3a236ad9223f4150dec35baabc
Brendan <bwoods51501@gmail.com>
2023-04-10 14:21:07 -0400

    Added Color

--- src/NodeGraph.java
+++ src/NodeGraph.java
@@ -1,10 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 
 public class NodeGraph extends JFrame {
 
     private JPanel panel;
+    private Set<Integer> visited;
+    private Queue<Integer> queue;
+    private GraphPanel graphPanel;
+    private int currentNode;
 
     public NodeGraph(int[][] adjacencyMatrix) {
         setTitle("Node Graph");
@@ -16,27 +22,38 @@ public class NodeGraph extends JFrame {
 
         setSize(500, 500);
 
-        setVisible(true);
-    }
-
-    public void bfs(int startNode, JPanel panel) {
-        GraphPanel graphPanel = (GraphPanel) panel;
-        Set<Integer> visited = new HashSet<>();
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(startNode);
-
-        while (!queue.isEmpty()) {
-            int currentNode = queue.poll();
-            visited.add(currentNode);
-            System.out.print(Character.toString((char)(currentNode+65)) + " ");
-
-            for (int i = 0; i < graphPanel.numNodes; i++) {
-                if (graphPanel.adjacencyMatrix[currentNode][i] == 1 && !visited.contains(i)) {
-                    queue.add(i);
-                    visited.add(i);
+        JButton nextButton = new JButton("Next");
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (queue.isEmpty()) {
+                    System.out.println("BFS traversal completed");
+                    return;
                 }
+
+                currentNode = queue.poll();
+                visited.add(currentNode);
+                System.out.print(Character.toString((char) (currentNode + 65)) + " ");
+
+                for (int i = 0; i < graphPanel.numNodes; i++) {
+                    if (graphPanel.adjacencyMatrix[currentNode][i] == 1 && !visited.contains(i)) {
+                        queue.add(i);
+                        visited.add(i);
+                    }
+                }
+
+                graphPanel.currentNode = currentNode;
+                panel.repaint();
             }
-        }
+        });
+        add(nextButton, BorderLayout.SOUTH);
+
+        setVisible(true);
+
+        graphPanel = (GraphPanel) panel;
+        visited = new HashSet<>();
+        queue = new LinkedList<>();
+        queue.add(0); // Starting node
     }
 
     public static void main(String[] args) {
@@ -48,7 +65,6 @@ public class NodeGraph extends JFrame {
                 {1, 1, 0, 1, 0}
         };
         NodeGraph graph = new NodeGraph(adjacencyMatrix);
-        graph.bfs(0, graph.panel);
     }
 
     private class GraphPanel extends JPanel {
@@ -58,6 +74,7 @@ public class NodeGraph extends JFrame {
         private int[][] adjacencyMatrix;
         private int numNodes;
         private Map<Integer, Point> nodePositions;
+        private int currentNode;
 
         public GraphPanel(int[][] adjacencyMatrix) {
             this.adjacencyMatrix = adjacencyMatrix;
@@ -65,35 +82,45 @@ public class NodeGraph extends JFrame {
             this.nodePositions = new HashMap<>();
         }
 
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
-            Random random = new Random();
-            for (int i = 0; i < numNodes; i++) {
-                int x = random.nextInt(getWidth() - NODE_SIZE);
-                int y = random.nextInt(getHeight() - NODE_SIZE);
-                nodePositions.put(i, new Point(x, y));
-            }
+        Random random = new Random();
+        for (int i = 0; i < numNodes; i++) {
+            int x = random.nextInt(getWidth() - NODE_SIZE);
+            int y = random.nextInt(getHeight() - NODE_SIZE);
+            nodePositions.put(i, new Point(x, y));
+        }
 
-            for (int i = 0; i < numNodes; i++) {
-                for (int j = i + 1; j < numNodes; j++) {
-                    if (adjacencyMatrix[i][j] == 1) {
-                        Point node1 = nodePositions.get(i);
-                        Point node2 = nodePositions.get(j);
+        for (int i = 0; i < numNodes; i++) {
+            for (int j = i + 1; j < numNodes; j++) {
+                if (adjacencyMatrix[i][j] == 1) {
+                    Point node1 = nodePositions.get(i);
+                    Point node2 = nodePositions.get(j);
 
-                        g.drawLine(node1.x + NODE_SIZE / 2, node1.y + NODE_SIZE / 2,
-                                node2.x + NODE_SIZE / 2, node2.y + NODE_SIZE / 2);
-                    }
+                if (visited.contains(i) && visited.contains(j)) {
+                    g.setColor(Color.RED);
+                } else {
+                    g.setColor(Color.BLACK);
+                }
+                g.drawLine(node1.x + NODE_SIZE / 2, node1.y + NODE_SIZE / 2,
+                        node2.x + NODE_SIZE / 2, node2.y + NODE_SIZE / 2);
                 }
             }
+        }
 
-            for (int i = 0; i < numNodes; i++) {
-                Point node = nodePositions.get(i);
-                g.fillOval(node.x, node.y, NODE_SIZE, NODE_SIZE);
-                g.setColor(Color.WHITE);
-                g.drawString(Character.toString((char)(i+65)), node.x + NODE_SIZE / 2 - 4, node.y + NODE_SIZE / 2 + 4);
+        for (int i = 0; i < numNodes; i++) {
+            Point node = nodePositions.get(i);
+            if (visited.contains(i)) {
+                g.setColor(Color.RED);
+            } else {
                 g.setColor(Color.BLACK);
+            }
+            g.fillOval(node.x, node.y, NODE_SIZE, NODE_SIZE);
+            g.setColor(Color.WHITE);
+            g.drawString(Character.toString((char)(i+65)), node.x + NODE_SIZE / 2 - 4, node.y + NODE_SIZE / 2 + 4);
+            g.setColor(Color.BLACK);
             }
         }
     }
